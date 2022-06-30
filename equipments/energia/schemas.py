37ff8347ef581bef01fddb3d7968e4bced5c67a6
Christian <christian.cruvinel.franca@gmail.com>
2022-06-29 23:16:16 -0300

    Adicionados Schemas para UPS e CTAs

--- equipments/energia/schemas.py
+++ equipments/energia/schemas.py
@@ -23,6 +23,37 @@ class GeradorRequest(BaseModel):
     dataInstalacao: Optional[date] = Body(None)
     infAdicional: Optional[str] = Body(None)
 
+class CTARequest(BaseModel):
+    tag: str
+    corrente: Optional[float] = Body(None)
+    fabricante: Optional[str] = Body(None)
+    modelo: Optional[str] = Body(None)
+    numSerie: Optional[str] = Body(None)
+    local: str
+    dataFabricacao: Optional[date] = Body(None)
+    dataInstalacao: Optional[date] = Body(None)
+    infAdicional: Optional[str] = Body(None)
+    
+class UPSRequest(BaseModel):
+    tag: str
+    potencia: Optional[float] = Body(None)
+    fabricante: Optional[str] = Body(None)
+    modelo: Optional[str] = Body(None)
+    numSerie: Optional[str] = Body(None)
+    instalador: Optional[str] = Body(None)
+    fabricanteBateria: Optional[str] = Body(None)
+    modeloBateria: Optional[str] = Body(None)
+    dataInstBateria: Optional[date] = Body(None)
+    tensaoSaida: Optional[float] = Body(None)
+    qtdeMonoblocos: Optional[int] = Body(None)
+    tensaoMonoblocos: Optional[float] = Body(None)
+    local: str
+    dataFabricacao: Optional[date] = Body(None)
+    dataInstalacao: Optional[date] = Body(None)
+    infAdicional: Optional[str] = Body(None)
+
 ROUTE_SCHEMAS_DICT = {
             'geradores': GeradorRequest,
+            'ctas': CTARequest,
+            'ups': UPSRequest
         }
