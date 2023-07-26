543861c312f263103d9b59cd6e7aab2926f5f438
Christian Cruvinel <christian.cruvinel.franca@gmail.com>
2023-07-26 11:43:43 -0300

    Adicionados exaustores

--- equipments/ventilacao/schemas.py
+++ equipments/ventilacao/schemas.py
@@ -12,7 +12,7 @@ class VentiladorRequest(BaseModel):
     modelo: Optional[str] = Body(None)
     vazao: Optional[float] = Body(None)
     pressaoEstaticaDisponivel: Optional[float] = Body(None)
-    tipoVentilador: Optional[str] = Body(None)
+    tipo: Optional[str] = Body(None)
     potenciaMotor: Optional[float] = Body(None)
     polia: Optional[str] = Body(None)
     tensao: Optional[int] = Body(None)
@@ -22,7 +22,26 @@ class VentiladorRequest(BaseModel):
     local: str
     infAdicional: Optional[str] = Body(None)
 
+class ExaustorRequest(BaseModel):
+    tag: str
+    pavimento: str
+    torre: str
+    ala: Optional[str] = Body(None)
+    fabricante: Optional[str] = Body(None)
+    modelo: Optional[str] = Body(None)
+    vazao: Optional[float] = Body(None)
+    pressaoEstaticaDisponivel: Optional[float] = Body(None)
+    tipo: Optional[str] = Body(None)
+    potenciaMotor: Optional[float] = Body(None)
+    polia: Optional[str] = Body(None)
+    tensao: Optional[int] = Body(None)
+    tipoEnergia: Optional[str] = Body(None)
+    dataFabricacao: Optional[date] = Body(None)
+    dataInstalacao: Optional[date] = Body(None)
+    local: str
+    infAdicional: Optional[str] = Body(None)
 
 ROUTE_SCHEMAS_DICT = {
-            'ventiladores': VentiladorRequest
+            'ventiladores': VentiladorRequest,
+            'exaustores': ExaustorRequest
         }
